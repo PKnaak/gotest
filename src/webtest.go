18ae9b00a1a83d3e76b494717f1867b590584d82
Rob Kluin <rob@twtg.io>
2017-12-21 17:33:46 +0100

    update webtest

--- src/webtest.go
+++ src/webtest.go
@@ -25,16 +25,20 @@ func loadPage(title string) (*Page, error) {
     return &Page{Title: title, Body: body}, nil
 }
 
-func handler(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "Sup son ¯\\_(ツ)_/¯  %s", r.URL.Path[1:])
+func viewHandler(w http.ResponseWriter, r *http.Request) {
+    title := r.URL.Path[len("/view/"):]
+    p, _ := loadPage(title)
+    fmt.Fprintf(w, "<h1>%s</h1><div>%s</div>", p.Title, p.Body)
 }
 
-func main() {
-	/*p1 := &Page{Title: "TestPage", Body: []byte("This is a sample Page.")}
-    p1.save()
-    p2, _ := loadPage("TestPage")
-    fmt.Println(string(p2.Body))*/
+/*
+func handler(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintf(w, "Sup son ¯\\_(ツ)_/¯  %s", r.URL.Path[1:])
+}*/
 
-    http.HandleFunc("/", handler)
-    http.ListenAndServe(":8080", nil)
+func main() {
+	http.HandleFunc("/view/", viewHandler)
+
+	fmt.Printf("Started!\r\n")
+	http.ListenAndServe(":8080", nil)
 }
